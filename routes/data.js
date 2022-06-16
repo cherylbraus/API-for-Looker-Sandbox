871e2e4d64de6c1c81cef87f121a695e412da2e7
petermdamato <petermdamato@gmail.com>
2022-06-16 19:08:32 -0400

    two pivots

--- routes/data.js
+++ routes/data.js
@@ -1,6 +1,7 @@
 const dataRoutes = (app, fs) => {
   // variables
   const dataPathOne = './data/dataOnePivot.json';
+  const dataPathTwoPiv = './data/dataTwoPivots.json';
   const dataPathTwo = './data/dataTwoDimensions.json';
 
   // READ
@@ -16,6 +17,16 @@ const dataRoutes = (app, fs) => {
 
     app.get('/dataTwoDimensions', (req, res) => {
     fs.readFile(dataPathTwo, 'utf8', (err, data) => {
+      if (err) {
+        throw err;
+      }
+
+      res.send(JSON.parse(data));
+    });
+  });
+
+        app.get('/dataTwoPivots', (req, res) => {
+    fs.readFile(dataPathTwoPivots, 'utf8', (err, data) => {
       if (err) {
         throw err;
       }
