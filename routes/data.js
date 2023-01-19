1a249866ed33d017d084ee92d6ca709a79b2442f
cherylbraus <cherylbraus@transfix.io>
2023-01-19 12:35:11 -0500

    2nd contractwinrate data

--- routes/data.js
+++ routes/data.js
@@ -42,6 +42,7 @@ const dataRoutes = (app, fs) => {
   const dataPathSankey3 = './data/dataSankey3.json';
   // const dataPathASP = './data/asp_stub.csv';
   const dataPathConWinRate = './data/dataConWinRate.json';
+  const dataPathConWinRate2 = './data/dataConWinRate2.json';
 
   // READ
   app.get('/dataOnePivot', (req, res) => {
@@ -446,6 +447,16 @@ const dataRoutes = (app, fs) => {
 
   app.get('/dataConWinRate', (req, res) => {
     fs.readFile(dataPathConWinRate, 'utf8', (err, data) => {
+      if (err) {
+        throw err;
+      }
+
+      res.send(JSON.parse(data));
+    });
+  });
+
+  app.get('/dataConWinRate2', (req, res) => {
+    fs.readFile(dataPathConWinRate2, 'utf8', (err, data) => {
       if (err) {
         throw err;
       }
